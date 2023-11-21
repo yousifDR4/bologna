184ecbda9dd30324989efbe887f6f09a92c7015d
unknown <hassun591@gmail.com>
2023-11-21 17:54:28 +0300

    hook

---
+++ hooks/useOneFetch.jsx
@@ -0,0 +1,25 @@
+import { doc  ,getDoc} from "firebase/firestore"
+import { auth ,db} from "../store/fire"
+import { useEffect, useState } from "react"
+export const useFetch=(id)=>{
+const [data,setData]=useState([]);
+const [load,setload]=useState(false);
+const [error,setError]=useState(false);
+useEffect(() => {
+    const f = async () => {
+        setload(true)
+      if (!auth.currentUser) return;
+      try {
+        const temp=await getDoc(doc(db,"users",id));
+        let data=temp.data()
+        setData(data);
+        setload(false);
+       }
+       catch (e) {
+        setError(true)
+      }
+    };
+    f();
+  }, [auth.currentUser,id]);
+  return{data,load,error}
+}
