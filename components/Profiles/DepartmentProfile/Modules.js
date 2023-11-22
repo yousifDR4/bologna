e0b49e633844932dcf5cf1ac40b3afc9783d61c7
unknown <hassun591@gmail.com>
2023-11-22 22:00:45 +0300

    k

--- components/Profiles/DepartmentProfile/Modules.js
+++ components/Profiles/DepartmentProfile/Modules.js
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LevelModule from "./LevelModule";
 import classes from "./Modules.module.css";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { auth ,db} from "../../../store/fire";
+
 const Modules=(probs)=>{
+
     const [levels,setLevels]=useState(0);
+    useEffect(()=>{
+        if (!auth.currentUser) return; 
+     const f=async()=>{
+      
+            const level=(await getDoc(doc(db,"users",auth.currentUser.uid))).get("levels");
+            setLevels(level)
+            console.log(level);
+     }
+     f();
+    },[auth.currentUser])
+   
     const levelsContainer=[];
     for (let i = 1; i <= levels; i++) {
         levelsContainer.push(<LevelModule key={i} level={i}/>);
     }
-    const addLevel=()=>{
+    const addLevel=async()=>{
         setLevels((prev)=>{
+            setDoc(doc(db,"users",auth.currentUser.uid),{levels:prev+1},{merge:true})
             return prev=prev+1;
         });
+        
     }
     return(
         <ul className={classes.modulesList}>
